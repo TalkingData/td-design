6c59dce1b4c2ae70346ff66fb4a3c90b771c0467
lamphc <lamphc@live.com>
2019-07-08 16:12:52 +0800

    feat: 优化放大器渲染

--- src/directives/magnifier.js
+++ src/directives/magnifier.js
@@ -13,15 +13,25 @@ Vue.directive("magnifier", {
     box.addEventListener("click", t => {
       const tar = t.target;
       if (tar.tagName === "IMG") {
+        const shadow = document.getElementById("fimg-shadow");
         if (tar.className.indexOf("fimg-show") > -1) {
           tar.className = "fimg-cursor";
-          document.body.removeChild(document.getElementById("fimg-shadow"));
+          document.body.className = "";
+          shadow && (shadow.style.display = "none");
+          // document.body.removeChild(shadow);
         } else {
           tar.className = "fimg-show";
-          const div = document.createElement("div");
-          div.id = "fimg-shadow";
-          div.style.height = el.offsetHeight + 1000 + "px";
-          document.body.appendChild(div);
+          let curOffh = el.offsetHeight + 1000 + "px";
+          if (!shadow) {
+            const div = document.createElement("div");
+            div.id = "fimg-shadow";
+            div.style.height = curOffh;
+            document.body.appendChild(div);
+          } else {
+            shadow.style.height = curOffh;
+            shadow.style.display = "block";
+          }
+          document.body.className = "ohd";
         }
       }
     });
