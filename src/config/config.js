1c5eea91d5ddcb01a6e7a7802ff8b8000b19e322
梁灏 <admin@aresn.com>
2019-07-09 16:31:57 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ if (env === "production") {
   baseUrl.rootPath = "http://172.28.4.230:7001";
 } else {
   baseUrl.rootPath = "http://172.28.4.230:7001";
+  // baseUrl.rootPath = "http://127.0.0.1:7001";
 }
 
 export { baseUrl };
