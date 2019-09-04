34ee32e4f69f1da7f0ba9f3b117ad0eef97b8391
pengfei.liu <pengfei.liu@tendcloud.com>
2019-09-04 16:26:00 +0800

    http-master

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,14 @@ const baseUrl = {};
 
 if (env === "production") {
   // baseUrl.rootPath = "http://172.28.4.230:7001";
-  baseUrl.rootPath = "http://172.29.3.19:7001";
+  //baseUrl.rootPath = "http://172.29.3.19:7001";
+  baseUrl.rootPath = "http://design.talkingdata.com";
 } else {
   // baseUrl.rootPath = "http://172.28.4.230:7001";
-  baseUrl.rootPath = "http://172.29.3.19:7001";
+  // baseUrl.rootPath = "http://172.29.3.19:7001";
+  baseUrl.rootPath = "http://design.talkingdata.com"
 }
 
-export { baseUrl };
+export {
+  baseUrl
+};
